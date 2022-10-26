d39aafd6b7149ffd640bf64325144ba5efd81cee
hulyhulyhuly <33054807+hulyhulyhuly@users.noreply.github.com>
2022-10-26 08:32:23 +0800

    update; make it better

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+""" Shopee Tool """
 import sys
 import os
 from openpyxl.styles import PatternFill, Font, Border, Side, Alignment
@@ -8,56 +9,51 @@ date_today = input("請輸入今天的日期：")
 date_begin = input("請輸入報表的起始日期：")
 date_end = input("請輸入報表的結束日期：")
 
-orders_folder = input("請輸入報表存放的資料夾：")
-
-template_path = input("請輸入模板存放的資料夾：")
-
-template_file_name = 'Shopee_template.xlsx'
+ORDERS_FOLDER = input("請輸入報表存放的資料夾：")
+TEMPLATE_PATH = input("請輸入模板存放的資料夾：")
+TEMPLATE_FILE_NAME = 'Shopee_template.xlsx'
 
 # 路徑懶得處理 不要有中文
-
-today_orders = f'Order.all.2022{date_begin}_2022{date_end}.xlsx'
-file_path = os.path.join(orders_folder, today_orders)
+TODAY_ORDERS = f'Order.all.2022{date_begin}_2022{date_end}.xlsx'
+TODAY_ORDERS_FILE_PATH = os.path.join(ORDERS_FOLDER, TODAY_ORDERS)
 
 # 今日訂單存放位置
-save_folder_path = os.path.join(orders_folder, f'2022{date_today}')
-if not os.path.exists(save_folder_path):
-    os.mkdir(save_folder_path)
+SAVE_FOLDER_PATH = os.path.join(ORDERS_FOLDER, f'2022{date_today}')
+if not os.path.exists(SAVE_FOLDER_PATH):
+    os.mkdir(SAVE_FOLDER_PATH)
 
-os.chdir(orders_folder)
+os.chdir(ORDERS_FOLDER)
 
 # 讀取報表
-wb = load_workbook(file_path)
+wb = load_workbook(TODAY_ORDERS_FILE_PATH)
 
 # 指定第一個工作頁
-order_sheet = wb[wb.sheetnames[0]]
+orders_sheet = wb[wb.sheetnames[0]]
 
 # 自訂欄位順序
 sort_target_cols_lst = [
     1, 39, 34, 33, 6, 12, 13, 14, 16, 18, 8, 24, 25, 29, 30, 27, 32, 11, 49, 50, 2, 40
 ]
 
-consumer_lst = [[0 for x in range(len(sort_target_cols_lst))] for y in range(order_sheet.max_row)]
+consumer_lst = [[0 for _ in range(len(sort_target_cols_lst))] for _ in range(orders_sheet.max_row)]
 
 # 欄位標題 放進去
-for col in range(len(sort_target_cols_lst)):
-    consumer_lst[0][col] = order_sheet.cell(1, sort_target_cols_lst[col]).value
+for col, _ in enumerate(sort_target_cols_lst):
+    consumer_lst[0][col] = orders_sheet.cell(1, sort_target_cols_lst[col]).value
 
 # 訂單資料
-for row in range(order_sheet.max_row):
+for row in range(orders_sheet.max_row):
 
     # 只要 "待出貨" 訂單
     # 直接 continue 所以這行都是空的
-    if order_sheet.cell(row + 1, 2).value != "待出貨":
+    if orders_sheet.cell(row + 1, 2).value != "待出貨":
         continue
 
     # 按照我要的順序塞進 list (順序 與 template 的欄位順序相關)
-    for col in range(len(sort_target_cols_lst)):
-        consumer_lst[row][col] = order_sheet.cell(row + 1, sort_target_cols_lst[col]).value
+    for col, _ in enumerate(sort_target_cols_lst):
+        consumer_lst[row][col] = orders_sheet.cell(row + 1, sort_target_cols_lst[col]).value
 
 wb.close()
-
-y = 1
 
 # 用到 6 欄 A ~ F
 title_lst = ['A', 'B', 'C', 'D', 'E', 'F']
@@ -65,23 +61,26 @@ title_lst = ['A', 'B', 'C', 'D', 'E', 'F']
 # 中等粗細的框線 黑色
 medium = Side(border_style="medium", color="000000")
 
-while y < len(consumer_lst):
+# 第 0 列 是欄位標題
+# 從第 1 列開始
+ROW = 1
+while ROW < len(consumer_lst):
     # 【待出貨】以外的訂單
     # 前面塞資料時，直接略過，所以都沒有資料
     # 這邊也略過不處理，直接進到下一筆訂單
-    if not consumer_lst[y][0]:
-        y += 1
+    if not consumer_lst[ROW][0]:
+        ROW += 1
         continue
 
     # 讀取模板
-    sole_order = load_workbook(os.path.join(template_path, template_file_name))
+    sole_order = load_workbook(os.path.join(TEMPLATE_PATH, TEMPLATE_FILE_NAME))
 
     # 指定第一個工作頁
     current_order = sole_order[sole_order.sheetnames[0]]
 
     # 訂單編號 收件者姓名 收件者電話 收件者地址 訂單日期 出貨分店(這欄手動添加)
     for i in range(5):
-        current_order[f'{title_lst[i]}2'] = consumer_lst[y][i]
+        current_order[f'{title_lst[i]}2'] = consumer_lst[ROW][i]
         current_order[f'{title_lst[i]}2'].alignment = Alignment(vertical="center")
         current_order[f'{title_lst[i]}2'].font = Font(name='微軟正黑體')
 
@@ -95,37 +94,37 @@ while y < len(consumer_lst):
 
     # 蝦皮補貼金額 蝦幣折抵 銀行信用卡活動折抵 賣場優惠券 優惠券 買家支付運費
     for i in range(5, 11):
-        current_order[f'{title_lst[i-5]}4'] = consumer_lst[y][i]
+        current_order[f'{title_lst[i-5]}4'] = consumer_lst[ROW][i]
         current_order[f'{title_lst[i-5]}4'].font = Font(name='Calibri')
         current_order[f'{title_lst[i-5]}4'].alignment = Alignment(horizontal="right",
                                                                   vertical="center")
 
     # 商品名稱 商品選項名稱 商品選項貨號 數量 商品活動價格 蝦皮促銷組合折扣:促銷組合標籤
-    same_order_row = y
-    same_order_count = 0
-    cur_order_num = consumer_lst[y][0]
+    SAME_ORDER_ROW = ROW
+    SAME_ORDER_COUNT = 0
+    cur_order_num = consumer_lst[ROW][0]
 
     # 統計一次買多少件商品
-    if same_order_row + 1 != len(consumer_lst):
-        while consumer_lst[same_order_row + 1][0] == cur_order_num:
-            if same_order_row == len(consumer_lst):
+    if SAME_ORDER_ROW + 1 != len(consumer_lst):
+        while consumer_lst[SAME_ORDER_ROW + 1][0] == cur_order_num:
+            if SAME_ORDER_ROW == len(consumer_lst):
                 break
-            same_order_count += 1
-            same_order_row += 1
+            SAME_ORDER_COUNT += 1
+            SAME_ORDER_ROW += 1
 
-    goods_index_begin = 6
-    goods_index_end = 6 + same_order_count
+    GOODS_INDEX_BEGIN = 6
+    GOODS_INDEX_END = 6 + SAME_ORDER_COUNT
 
-    for g in range(goods_index_begin, goods_index_end + 1):
+    for g in range(GOODS_INDEX_BEGIN, GOODS_INDEX_END + 1):
         for i in range(11, 17):
-            current_order[f'{title_lst[i-11]}{g}'] = consumer_lst[y + g - 6][i]
+            current_order[f'{title_lst[i-11]}{g}'] = consumer_lst[ROW + g - 6][i]
             current_order[f'{title_lst[i-11]}{g}'].alignment = Alignment(vertical="center")
             current_order[f'{title_lst[i-11]}{g}'].border = Border(top=medium,
                                                                    left=medium,
                                                                    right=medium,
                                                                    bottom=medium)
 
-    for g in range(goods_index_begin, goods_index_end + 1):
+    for g in range(GOODS_INDEX_BEGIN, GOODS_INDEX_END + 1):
         current_order[f'A{g}'] = current_order[f'A{g}'].value.replace(" ToysRUs玩具反斗城", "")
         current_order[f'A{g}'].alignment = Alignment(wrap_text=True)
 
@@ -139,68 +138,73 @@ while y < len(consumer_lst):
         current_order[f'F{g}'].alignment = Alignment(horizontal="center", vertical="center")
 
     # 買家總支付金額
-    current_order[f'A{goods_index_end + 1}'] = consumer_lst[0][17]
-    current_order[f'A{goods_index_end + 1}'].alignment = Alignment(vertical="center")
-    current_order[f'A{goods_index_end + 1}'].fill = PatternFill(start_color="FFFF00",
+    current_order[f'A{GOODS_INDEX_END + 1}'] = consumer_lst[0][17]
+    current_order[f'A{GOODS_INDEX_END + 1}'].alignment = Alignment(vertical="center")
+    current_order[f'A{GOODS_INDEX_END + 1}'].fill = PatternFill(start_color="FFFF00",
                                                                 fill_type="solid")
 
-    current_order[f'E{goods_index_end + 1}'] = consumer_lst[y][17]
-    current_order[f'E{goods_index_end + 1}'].alignment = Alignment(horizontal="right",
+    current_order[f'E{GOODS_INDEX_END + 1}'] = consumer_lst[ROW][17]
+    current_order[f'E{GOODS_INDEX_END + 1}'].alignment = Alignment(horizontal="right",
                                                                    vertical="center")
 
     for i in range(6):
-        current_order[f'{title_lst[i]}{goods_index_end + 1}'].border = Border(top=medium,
+        current_order[f'{title_lst[i]}{GOODS_INDEX_END + 1}'].border = Border(top=medium,
                                                                               left=medium,
                                                                               right=medium,
                                                                               bottom=medium)
 
     # 買家備註
-    if consumer_lst[y][18]:
-        current_order[f'A{goods_index_end + 3}'] = consumer_lst[0][18]
-        current_order[f'A{goods_index_end + 3}'].font = Font(color="FFFFFF", bold=True)
-        current_order[f'A{goods_index_end + 3}'].fill = PatternFill(start_color="C00000",
+    if consumer_lst[ROW][18]:
+        current_order[f'A{GOODS_INDEX_END + 3}'] = consumer_lst[0][18]
+        current_order[f'A{GOODS_INDEX_END + 3}'].font = Font(color="FFFFFF", bold=True)
+        current_order[f'A{GOODS_INDEX_END + 3}'].fill = PatternFill(start_color="C00000",
                                                                     fill_type="solid")
-        current_order[f'A{goods_index_end + 3}'].border = Border(top=medium,
+        current_order[f'A{GOODS_INDEX_END + 3}'].border = Border(top=medium,
                                                                  left=medium,
                                                                  right=medium,
                                                                  bottom=medium)
 
-        current_order[f'A{goods_index_end + 4}'] = consumer_lst[y][18]
-        current_order[f'A{goods_index_end + 4}'].font = Font(name='微軟正黑體')
-        current_order[f'A{goods_index_end + 4}'].alignment = Alignment(vertical="center")
-        current_order[f'A{goods_index_end + 4}'].border = Border(top=medium,
+        current_order[f'A{GOODS_INDEX_END + 4}'] = consumer_lst[ROW][18]
+        current_order[f'A{GOODS_INDEX_END + 4}'].font = Font(name='微軟正黑體')
+        current_order[f'A{GOODS_INDEX_END + 4}'].alignment = Alignment(vertical="center")
+        current_order[f'A{GOODS_INDEX_END + 4}'].border = Border(top=medium,
                                                                  left=medium,
                                                                  right=medium,
                                                                  bottom=medium)
 
     # 備註
-    if consumer_lst[y][19]:
-        current_order[f'C{goods_index_end + 3}'] = consumer_lst[0][19]
-        current_order[f'C{goods_index_end + 3}'].font = Font(color="FFFFFF")
-        current_order[f'C{goods_index_end + 3}'].fill = PatternFill(start_color="C00000",
+    if consumer_lst[ROW][19]:
+        current_order[f'C{GOODS_INDEX_END + 3}'] = consumer_lst[0][19]
+        current_order[f'C{GOODS_INDEX_END + 3}'].font = Font(color="FFFFFF")
+        current_order[f'C{GOODS_INDEX_END + 3}'].fill = PatternFill(start_color="C00000",
                                                                     fill_type="solid")
-        current_order[f'C{goods_index_end + 3}'].border = Border(top=medium,
+        current_order[f'C{GOODS_INDEX_END + 3}'].border = Border(top=medium,
                                                                  left=medium,
                                                                  right=medium,
                                                                  bottom=medium)
 
-        current_order[f'C{goods_index_end + 4}'] = consumer_lst[y][19]
-        current_order[f'C{goods_index_end + 4}'].font = Font(name='微軟正黑體')
-        current_order[f'C{goods_index_end + 4}'].alignment = Alignment(vertical="center")
-        current_order[f'C{goods_index_end + 4}'].border = Border(top=medium,
+        current_order[f'C{GOODS_INDEX_END + 4}'] = consumer_lst[ROW][19]
+        current_order[f'C{GOODS_INDEX_END + 4}'].font = Font(name='微軟正黑體')
+        current_order[f'C{GOODS_INDEX_END + 4}'].alignment = Alignment(vertical="center")
+        current_order[f'C{GOODS_INDEX_END + 4}'].border = Border(top=medium,
                                                                  left=medium,
                                                                  right=medium,
                                                                  bottom=medium)
 
     # 寄出方式
-    shipping_method = '全家' if consumer_lst[y][-1] == '全家' else '宅配'
+    SHIPPING_METHOD = '全家' if consumer_lst[ROW][-1] == '全家' else '宅配'
 
     # 訂單成立時間
-    order_date = str(consumer_lst[y][4]).split(" ")[0]
+    CURRENT_ORDER_DATE = str(consumer_lst[ROW][4]).split(" ", maxsplit=1)[0]
 
-    file_name = f'Shopee_{order_date}_{shipping_method}_{str(consumer_lst[y][0])}_{str(consumer_lst[y][1]).replace("*","x")}.xlsx'
-    save_file_path = os.path.join(save_folder_path, file_name)
-    sole_order.save(save_file_path)
+    # 買家姓名
+    CONSUMER_NAME = str(consumer_lst[ROW][1]).replace("*", "x")
+
+    CURRENT_ORDER_FILE_NAME = f'Shopee_{CURRENT_ORDER_DATE}_{SHIPPING_METHOD}_\
+{str(consumer_lst[ROW][0])}_{CONSUMER_NAME}.xlsx'
+
+    SAVE_FILE_PATH = os.path.join(SAVE_FOLDER_PATH, CURRENT_ORDER_FILE_NAME)
+    sole_order.save(SAVE_FILE_PATH)
     sole_order.close()
 
-    y += same_order_count + 1
+    ROW += SAME_ORDER_COUNT + 1
